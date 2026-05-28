05e98cbd4f7ec9d6d5193d72063edfb27e8dd8da
unknown <2876390903@qq.com>
2026-05-28 23:21:58 +0800

    修复部署配置

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2026-04-20 17:09:10
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2026-05-28 23:19:02
+ * @FilePath: \undefinedc:\Users\TR\Desktop\blong\vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -6,6 +14,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/博客系统',
   plugins: [
     vue(),
     vueDevTools(),
