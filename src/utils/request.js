7cddc7ed9cfe253c27e42766c99b96231b5c7602
asjdbx <131530593+asjdbx@users.noreply.github.com>
2026-06-10 21:41:36 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -13,6 +13,21 @@
  * @LastEditTime: 2025-07-02 18:07:09
  * @FilePath: \blong\src\utils\request.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-07-02 16:53:53
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2026-06-10 21:32:11
+ * @FilePath: \blong\src\utils\request.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-07-01 22:53:11
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-07-02 18:07:09
+ * @FilePath: \blong\src\utils\request.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 //定制请求的实例
 
@@ -20,33 +35,27 @@
 import axios from 'axios';
 
 import { ElMessage } from 'element-plus'
+
+
 // 1. 创建axios实例service（缺少这行就报service未定义）
 const service = axios.create({
   baseURL: '',
   timeout: 5000
 })
 
-// 响应错误拦截（git page只能静态页面，拦截不报错）
-// service.interceptors.response.use(
-//   res => res.data,
-//   err => {
-//     console.log('接口异常', err)
-//     //  只有打包上线PROD才返回假数据、关闭弹窗
-//     if (import.meta.env.PROD) {
-//       return Promise.resolve({ code: 200, data: [] })
-//     }
-//     // 本地DEV：正常抛出错误，页面弹出服务异常（本地需要弹窗提示接口崩了）
-//     return Promise.reject(err)
-//   }
-// )
+// // 请求拦截（可选）
+// service.interceptors.request.use(config => {
+//   return config
+// })
+//定义一个变量,记录公共的前缀  ,  baseURL
+//const baseURL = 'http://localhost:8080';
+const baseURL = '/api';
+const instance = axios.create({ baseURL })
 
-// //定义一个变量,记录公共的前缀  ,  baseURL
-// //const baseURL = 'http://localhost:8080';
-// const baseURL = '/api';
-// const instance = axios.create({ baseURL })
+import {useTokenStore} from '@/stores/token.js'
 
-// import {useTokenStore} from '@/stores/token.js'
-// //添加请求拦截器
+
+//添加请求拦截器
 // instance.interceptors.request.use(
 //     (config)=>{
 //         //请求前的回调
@@ -92,6 +101,18 @@ const service = axios.create({
 //         return Promise.reject(err);//异步的状态转化成失败的状态
 //     }
 // )
-
+// // 响应错误拦截（git page只能静态页面，拦截不报错）
+// service.interceptors.response.use(
+//   res => res.data,
+//   err => {
+//     console.log('接口异常', err)
+//     // ✅ 只有打包上线PROD才返回假数据、关闭弹窗
+//     if (import.meta.env.PROD) {
+//       return Promise.resolve({ code: 200, data: [] })
+//     }
+//     // ✅ 本地DEV：正常抛出错误，页面弹出服务异常（本地需要弹窗提示接口崩了）
+//     return Promise.reject(err)
+//   }
+// )
 
 export default instance;
